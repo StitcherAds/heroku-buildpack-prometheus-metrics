dfaafe882e02365125b840545dd2d99fcf59e56a
Paul Tobin <paul.tobin@stitcherads.com>
2023-02-08 17:34:07 +0000

    hardcode url and host, echo statements in .sh

--- script/statsd_prometheus_buildpack.sh
+++ script/statsd_prometheus_buildpack.sh
@@ -2,9 +2,15 @@
 
 # bash script to run the statsd_exporter and prometheus binaries
 # Path: script/run.sh
+echo "-----> Run statsd_prometheus_buildpack.sh"
+echo "-----> Run statsd_exporter"
 cd bin/statsd_exporter
 ./statsd_exporter & disown $!
+echo "-----> Run statsd_exporter is running"
 
-cd ../prometheus
-./prometheus --config.file=prometheus.yml & disown $!
 cd ~
+
+echo "-----> Run prometheus"
+cd bin/prometheus
+./prometheus --config.file=prometheus.yml & disown $!
+echo "-----> Run prometheus is running"
