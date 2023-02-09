040a451ae5b81704e2128af7953618b60847c04d
Paul Tobin <paul.tobin@stitcherads.com>
2023-02-09 13:58:35 +0000

    run statsd/prometheus in the background

--- script/statsd_prometheus_buildpack.sh
+++ script/statsd_prometheus_buildpack.sh
@@ -5,12 +5,14 @@
 echo "-----> Run statsd_prometheus_buildpack.sh"
 echo "-----> Run statsd_exporter"
 cd bin/statsd_exporter
-./statsd_exporter & disown $!
+./statsd_exporter& disown $!
 echo "-----> Run statsd_exporter is running"
 
 cd ~
 
 echo "-----> Run prometheus"
 cd bin/prometheus
-./prometheus --config.file=prometheus.yml & disown $!
+./prometheus& disown $!
 echo "-----> Run prometheus is running"
+
+cd ~
